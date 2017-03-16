a7df0ce5872b4ddea87787551293ef6f7c638ca4
sean-destefano <seangitdestefano@gmail.com>
2017-03-16 17:02:11 -0400

    Added basic script to install Chocolatey and normal package, as well as bind network location for later

--- NewDevice.ps1
+++ NewDevice.ps1
@@ -1,1 +1,7 @@
-##Script to setup new Device
+##Script to setup new Device after imaging
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install googlechrome vlc 7zip notepadplusplus malwarebytes adobereader ###make a menu?
+$Netloc = read-host -prompt 'Enter the network location'
+$Netcred = read-host -prompt 'Enter the domain\username for the network share'
+New-PSDrive -name R -psprovider FileSystem -Root $Netloc -persist -credential $Netcred
+#####file installation here?
