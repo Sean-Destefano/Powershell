6354661196728a99a3acb173db94356ad43da4a9
sean-destefano <seangitdestefano@gmail.com>
2017-03-29 13:01:44 -0400

    New Script

---
+++ New_User.ps1
@@ -0,0 +1,6 @@
+﻿Set-ExecutionPolicy bypass -force
+netsh wlan connect ssid="Prologic ITS" key="pr0l0gic"
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+choco install googlechrome 7zip notepadplusplus malwarebytes
+new-psdrive -name "K" -psprovider filesystem -root "\\10.0.2.173\S" -Persist
+start-process -FilePath "K:\New_User_Setup\Office\setup.exe"
